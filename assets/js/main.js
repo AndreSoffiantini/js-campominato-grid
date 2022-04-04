129cac539fee5407058a07db07b760883e4c7cb0
AndreSoffiantini <andrea.soffiantini@hotmail.it>
2022-04-04 16:53:26 +0200

    Add generateGrid function, error in Eventlistener

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,16 @@ con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro. */
+
+// Creare una funzione che al click del bottone Play generi una griglia quadrata
+function generateGrid(number_of_cells, container, element_name, class_name) {
+    const cellsContainer = document.querySelector(container);
+    for (let i = 1; i <= number_of_cells; i++) {
+        const cell = document.createElement(element_name);
+        cell.classList.add(class_name);
+        cellsContainer.append(cell);
+    }
+}
+
+const playBtn = document.getElementById("playBtn");
+playBtn.addEventListener("click", generateGrid(100, ".cells", "div", "cell"));
